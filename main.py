7e6ce9aa28025f50a5b13c27985303cfa99c97d3
Hemun143 <131951962+Hemun143@users.noreply.github.com>
2023-05-29 16:59:29 +0530

    First_Code

---
+++ main.py
@@ -0,0 +1,5 @@
+print("This is my first code in python")
+print(
+  "***************** Arthmatic expression with integar ans string  **********************"
+)
+print((1 + 1) * (50 / 3) + (2**8) - (45 - 4))
